173ef0004e77a81c4065bfa7632b857be4f46fbb
ziyad007 <ziyadloubaris@gmail.com>
2020-06-28 04:15:01 +0100

    modification de la manier de contacter

--- resources/views/donateur/homeDonateur.blade.php
+++ resources/views/donateur/homeDonateur.blade.php
@@ -53,7 +53,11 @@ Bonjour {{Auth::user()->nom}}
                         <td>{{$row->detail}}</td>
                         <td>{{$row->photo}}</td>
                         <td>
-                            <a href="/mess/{{$row->id}}/{{$user->email}}" class="btn btn-success">Contacter</a>
+                          <!--
+                          <a href="/mess/{{$row->id}}/{{$user->email}}" class="btn btn-success">Contacter</a>
+                          -->                          
+                           <a href="mailto:{{$user->email}} "class="btn btn-success" target="_blank">contacter</a>
+
                         </td>
                     </tr>  
                         @endif
